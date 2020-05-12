ea8d32964f62a212e10e876846411792adcb1998
Henrique Bonfim <hp_bonfim@hotmail.com>
2020-05-12 15:44:41 -0300

    convert to typescrypt

--- server.ts
+++ server.ts
@@ -1,31 +1,38 @@
 // server.js
 // where your node app starts
-var PORT = process.env.PORT || 3210
+const PORT: any = process.env.PORT || 3210
 // init project
-var express = require("express")
-var app = express()
+import express = require('express')
+const app: express.Application = express()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require("cors")
-app.use(cors({
-    optionSuccessStatus: 200
-})) // some legacy browsers choke on 204
+import cors = require("cors")
+
+const options: cors.CorsOptions = {
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+    credentials: true,
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    origin: "localhost", //API_URL
+    preflightContinue: false
+}
+
+app.use(cors(options)) // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"))
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function (req: any, res: any) {
     res.sendFile(__dirname + "/views/index.html")
 })
 
-app.get("/api/timestamp/:date_string?", (req, res) => {
+app.get("/api/timestamp/:date_string?", (req: any, res: any) => {
     console.log(req.params.date_string)
     console.log(typeof req.params.date_string)
-    let date
-    let unix
-    let utc
+    let date: any
+    let unix: any
+    let utc: any
 
     if (!req.params.date_string) {
         date = new Date()
@@ -53,6 +60,6 @@ app.get("/api/timestamp/:date_string?", (req, res) => {
 })
 
 // listen for requests :)
-var listener = app.listen(PORT, function () {
+const listener: any = app.listen(PORT, function () {
     console.log("Your app is listening on port " + listener.address().port)
 })
